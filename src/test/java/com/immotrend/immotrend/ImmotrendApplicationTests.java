dd010792a307b5951f2df8b7da5eb77879c3dceb
razzak <khalfallah.razzak@gmail.com>
2024-11-19 15:40:33 +0100

    second commit

---
+++ src/test/java/com/immotrend/immotrend/ImmotrendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.immotrend.immotrend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImmotrendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
